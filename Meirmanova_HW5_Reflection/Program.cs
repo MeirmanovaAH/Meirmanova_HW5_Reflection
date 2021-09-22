2ae14dc865efc77c9f87bfc4598f379b7bccc1d9
Akbota Meirmanova <meirmanova.akbota26@gmail.com>
2021-09-22 21:37:30 +0600

    Добавьте файлы проекта.

---
+++ Meirmanova_HW5_Reflection/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+
+namespace Meirmanova_HW5_Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 100;
+            Console.WriteLine($"Количество итераций CSV: {cnt}");
+            CSVSerializer(cnt);
+
+            Console.WriteLine("\n" + $"Количество итераций JSON: {cnt}");
+            JsonSerializer(cnt);
+        }
+
+        public static void CSVSerializer(int cnt)
+        {
+            var serSW = new Stopwatch();
+            var deserSW = new Stopwatch();
+            var ser = F.Get();
+
+            for (int i = 0; i < cnt; i++)
+            {
+                serSW.Start();
+                var csv = Serializer.SerializeToCsv(ser);
+                serSW.Stop();
+            }
+
+            Console.WriteLine($"Сериалиализация: {serSW.ElapsedMilliseconds} мс");
+            Console.WriteLine($"Десериалиализация: {deserSW.ElapsedMilliseconds} мс");
+        }
+
+        private static void JsonSerializer(int cnt)
+        {
+            var serSW = new Stopwatch();
+            var deserSW = new Stopwatch();
+
+            var instance = F.Get();
+            for (var i = 0; i < cnt; i++)
+            {
+                serSW.Start();
+                var csvContent = JsonConvert.SerializeObject(instance);
+                serSW.Stop();
+
+                deserSW.Start();
+                var deserialized = JsonConvert.DeserializeObject(csvContent, typeof(F));
+                deserSW.Stop();
+            }
+
+            Console.WriteLine($"Сериалиализация: {serSW.ElapsedMilliseconds} мс");
+            Console.WriteLine($"Десериалиализации: {deserSW.ElapsedMilliseconds} мс");
+        }
+    }
+}
---
+++ Meirmanova_HW5_Reflection/Serializer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Meirmanova_HW5_Reflection
+{
+    class Serializer
+    {
+        public static string ListSeparator = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ListSeparator;
+
+        public static string SerializeToCsv(object obj)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var field in obj.GetType().GetFields(System.Reflection.BindingFlags.Public))
+            {
+                sb.AppendLine($"{field.Name}{ListSeparator}{field.GetValue(obj)}");
+            }
+
+            return sb.ToString();
+        }
+
+        public static T DeserializeFromCsv<T>(string csv, T ob)
+        {
+            var type = ob.GetType();
+            var lines = csv.Split("\r\n").Where(line => !string.IsNullOrEmpty(line)).ToArray();
+
+            var obj = Activator.CreateInstance(type);
+
+            for (var i = 1; i < lines.Length; i++)
+            {
+                LoadFields(lines[i], obj);
+            }
+
+            return (T)obj;
+        }
+
+        private static void LoadFields(string field, object obj)
+        {
+            var parts = field.Split(ListSeparator);
+            var type = obj.GetType();
+            var fieldInfo = type.GetField(parts[0]);
+            fieldInfo.SetValue(field, obj);
+        }
+    }
+}
